39725cfca60bd32577036ddfa7c1a8cf27ff4576
trevor-duane@github.com <trevorkayiira@gmail.com>
2023-05-30 15:09:35 +0300

    Menu Data

--- src/pages/menu/Data.js
+++ src/pages/menu/Data.js
@@ -107,6 +107,7 @@ export const breakfastSandwiches = [
         "price":16000
     }
 ];
+
 export const miniBites = [
     {
         "id": 1,
@@ -189,12 +190,30 @@ export const plattersTwo = [
 export const Pastries = [
     {
         "id":1,
-        "name":"Cake",
+        "name":"Black Forest",
         "description":"A Single Slice",
         "price":10000
     },
     {
         "id":2,
+        "name":"White Forest",
+        "description":"A Single Slice",
+        "price":10000
+    },
+    {
+        "id":3,
+        "name":"Victoria Cake",
+        "description":"A Single Slice",
+        "price":10000
+    },
+    {
+        "id":4,
+        "name":"Chocolate Cake",
+        "description":"A Single Slice",
+        "price":10000
+    },
+    {
+        "id":5,
         "name":"Double Muffins",
         "description":"Either Banana, Chocolate, Vanila or Strawberry",
         "price":8000
@@ -214,4 +233,510 @@ export const Products = [
         "description":"Ground, Coffee Beans",
         "price":""
     },
+];
+
+//Lunch Menu Items
+export const Salads = [
+    {
+        "id":1,
+        "name":"Tenda House Mixed Green",
+        "description":"You will love our salad made, with green leaf lettuce, red cabbage, red onions, cucumber, tomato, carrots, avocado and green olives; served with our coffee french dressing",
+        "price":26000
+    },
+    {
+        "id":2,
+        "name":"Bacon Caesar Salad",
+        "description":"A salad fit for an emperor/queen. This one features savory bacon, lettuce, cucumbers, onions, tomatoes, garlic croutons, parmesan cheese and ofcourse caeser dressing",
+        "price":30000
+    },
+    {
+        "id":3,
+        "name":"Victoria Smoked Tilapia Salad",
+        "description":"Lake Victoria offers smoked fresh tilapia, a salad of green leafy lettuce and red cabbage, red onions, cucumber, tomato, avocado and black olives served with a coffee french dressing",
+        "price":28000
+    },
+    {
+        "id":4,
+        "name":"Spicy Chicken Salad",
+        "description":"Chilling with grilled chicken strips, this salad tops a bed of green leaf lettuce, tomatoes, red cabbage, red onions, sweet corn, green lives served with a coffee french dressing",
+        "price":30000
+    },
+    {
+        "id":5,
+        "name":"Chef's Special Cobb Salad",
+        "description":"We always keep the best for the last, this salad comes with crispy lettuce, tomatoes, avocado, chicken cubes, beef cubes, boiled egg, sweet corn and cheddar cheese served with honey mustard mayo",
+        "price":33000
+    },
+    {
+        "id":6,
+        "name":"Seasonal Fruit Salad",
+        "description":"This is an all-star cast of seasonal fruits favourites served all day, everyday, add honey, nuts and yougurt + 4000",
+        "price":16000
+    },
+    {
+        "id":7,
+        "name":"Seasonal Fruit Platter",
+        "description":"This here is a cast of seasonal fruit favourites served on a platter.",
+        "price":16000
+    }
+];
+
+export const Burgers = [
+    {
+        "id":1,
+        "name":"Beef Burger",
+        "description":"Absolute ground beef grilled to perfection topped with lettuce, tomato and onion on a toasted bun served with side salad and fries",
+        "price":29000
+    },
+    {
+        "id":2,
+        "name":"Chicken Burger",
+        "description":"Grilled chicken breast topped with lettuce, tomato and onion on a toasted bun served with side salad and fries",
+        "price":29000
+    },
+    {
+        "id":3,
+        "name":"Hawaiian Honolulu Burger",
+        "description":"Do not wait untill you get to Hawaii, this 100% ground beef or grilled chicken breast topped with charred ham, grilled pineapple, onions, lettuce, tomato, cheddar cheese and Hawaiian made BBQ sauce will take you there",
+        "price":29000
+    },
+    {
+        "id":4,
+        "name":"Millenial Burger",
+        "description":"This right here is the dotcom gen, 100% ground beef or grilled chicken breast topped with grilled onions, bacon, cheddar cheese, mushroom sauce, lettuce and tomato on a toasted bun",
+        "price":29000
+    },
+    {
+        "id":5,
+        "name":"Daddy's Big Burger",
+        "description":"Where's my burger, here it is 100% double ground beef or double grilled chicken breast topped, bacon, onion rings, jalapenos, egg, grilled onions, cheddar cheese, dressing, lettuce and tomato on a toasted bun",
+        "price":29000
+    },
+];
+
+export const ClassicSandwiches = [
+    {
+        "id": 1,
+        "name":"Triple Club Sandwich",
+        "description":"You don't need signing into this club just play with it's a double-decke of grilled chicken, bacon, egg, tomatoes, salads served with your choice of seasoned fries, house salads or steamed vegetables",
+        "price":33000
+    },
+    {
+        "id": 2,
+        "name":"Barbeque Stripper Sandwich",
+        "description":"Joy comes when things get a little barbequed, with our tender BBQ strips and classic cheddar served with your choice of seasoned fries, house salads or steamed vegetables",
+        "price":30000
+    },
+    {
+        "id": 3,
+        "name":"New York Cheese Steak",
+        "description":"In the beginning was steak then steak became a sandwich, this marinated steak filet, spicy sauteed vegetables and creamy cheddar cheese served with your seasoned fries, house salads or steamed vegetables",
+        "price":30000
+    },
+    {
+        "id": 4,
+        "name":"Smokehouse Tilapia Melt",
+        "description":"Fish loved in the room, dig into a double decker of smokey lake victoria tilapia, onions, tomatoes, lettuce, capers, avocado and mustard, served with seasoned fries, house salads or steamed vegetables",
+        "price":31000
+    },
+    {
+        "id": 5,
+        "name":"Hot Ham & Cheese",
+        "description":"The rumors are true, this hotty is for the BIG BOYS, with its grilled ham and melted spicy cheddar on grilled bread served with seasoned fries, house salads or steamed vegetables",
+        "price":26000
+    },
+    {
+        "id": 6,
+        "name":"Chicken Salad Sandwich",
+        "description":"Yes you had me right healthy, with celery, onions, tomatoes, cucumber, lettuce and chicken mayo on toasted bread served with seasoned fries, house salads or steamed vegetables",
+        "price":29000
+    },
+    {
+        "id": 7,
+        "name":"Classic Veggie Sandwich",
+        "description":"The prodigal sandwich returns to tenda cafe's menu with its double decker of grilled mushrooms, carrots, zucchini, onion, mustard mayo, mozarella cheese served with seasoned fries, house salads or setamed vegetables",
+        "price":28000
+    }
+];
+
+export const Soups = [
+    {
+        "id":1,
+        "name":"Classic Clear Chicken & Vegetable",
+        "price":12000
+    },
+    {
+        "id":2,
+        "name":"Clear Mushroom & Vegetables",
+        "price":12000
+    },
+    {
+        "id":3,
+        "name":"Cream of Mushroom Soup",
+        "price":12000
+    },
+    {
+        "id":4,
+        "name":"Cafe Tenda Smoky Fish & Vegetable Soup",
+        "price":13000
+    },
+    {
+        "id":5,
+        "name":"Cream of Chicken Soup",
+        "price":14000
+    },
+    {
+        "id":6,
+        "name":"Creampy Pumpkin Soup",
+        "price":14000
+    }
+];
+
+export const Sides = [
+    {
+        "id":1,
+        "name":"Plain Chips",
+        "price":9000
+    },
+    {
+        "id":2,
+        "name":"Seasonal Setamed Vegetables",
+        "price":12000
+    },
+    {
+        "id":3,
+        "name":"Mushroom Garlic Chips",
+        "price":12000
+    },
+    {
+        "id":4,
+        "name":"Masala Chips",
+        "price":12000
+    },
+    {
+        "id":5,
+        "name":"Sweet Plantain",
+        "price":10000
+    }
+];
+
+export const Addons = [
+    {
+        "id":1,
+        "name":"Pineapple",
+        "price":4000
+    },
+    {
+        "id":2,
+        "name":"Chicken Pattie",
+        "price":7000
+    },
+    {
+        "id":3,
+        "name":"Beef Burger Pattie",
+        "price":7000
+    },
+    {
+        "id":4,
+        "name":"Egg, Cheese, Mushroom",
+        "price":4000
+    },
+    {
+        "id":5,
+        "name":"Bacon",
+        "price":7000
+    }
+];
+
+export const TendaHouseCombinations = [
+    {
+        "id":1,
+        "name":"Basket Combination",
+        "description":"Joined like 2 samosa, 8 chicken wings and chapati, steamed veggies and gravy all married together on a platter served wit veggie rice",
+        "price":46000
+    },
+    {
+        "id":2,
+        "name":"Cafe Tenda Mixed Grill",
+        "description":"A generous helping of 1 piece steak, 1 piece chicken breast and 1 piece grilled fish served with house salads, veggie rice and gravy sauce",
+        "price":46000
+    },
+    {
+        "id":3,
+        "name":"Choma Sampler",
+        "description":"This is our way of bringing the EAST AFRICAN community to the table, this combination of sausages, steak, BBQ pork ribs, roasted chicken served with salads, veg rice and gravy",
+        "price":46000
+    },
+];
+
+export const Wraps = [
+    {
+        "id":1,
+        "name":"Savanna Chicken Wrap",
+        "description":"Uganda's, freshest flavours of chicken strips, mustard mayo, green leafy, lettuce, mixed salads and sweat chill served with fries",
+        "price":28000
+    },
+    {
+        "id":2,
+        "name":"Amazon Beef Wrap",
+        "description":"Journey to the amazon with our tender beef strips, mixed veggies, mushrooms, crispy lettuce and sweet chill served with fries of side",
+        "price":28000
+    },
+    {
+        "id":3,
+        "name":"Tropical Veggie Wrap",
+        "description":"Guess What? what of grilled mushrooms, carrots, zucchini, onion, mustard mayo, sweet chill and fries",
+        "price":26000
+    }
+];
+
+export const Fish = [
+    {
+        "id":1,
+        "name":"Lake Victoria Pan-Seared Grilled Fish",
+        "description":"Straight from lake victoria, pan-seared fish fillets grilled to perfection served with side salads, gravy and your choice of side",
+        "price":36000
+    },
+    {
+        "id":2,
+        "name":"South West English Style Fish",
+        "description":"Behold the queens favorite fish comes to town, this golden fried fish in abutter sauce served with tartar sauce and salads",
+        "price":36000
+    },
+    {
+        "id":3,
+        "name":"Mama's Whole Fish",
+        "description":"This is PROUDLY UGANDAN, fresh from lake victoria comes this perfectly cooked fish, prepared to customer style(fried or boiled) served with gravy, greens and ugali",
+        "price":45000
+    },
+    {
+        "id":4,
+        "name":"Mediterranean Fish",
+        "description":"Did you know that 21 countries make up this sea and all of them enjoy this fish cooked to perfection with herbs and spices served on a bed of sauteed vegetables and gravy",
+        "price":40000
+    },
+    {
+        "id":5,
+        "name":"Fish Fingers",
+        "description":"Finger-Lickin GOLD, breaded with herbs and spices served with salads and tartar sauce",
+        "price":33000
+    }
+];
+
+export const Curries = [
+    {
+        "id":1,
+        "name":"Hyderabad Beef Curry",
+        "description":"From the state of hyderabad india comes this carefully cooked beef curry served with steamed vegetables and the customers's choice of fries, mashed potatoes or steamed rice",
+        "price":35000
+    },
+    {
+        "id":2,
+        "name":"Malaysian Coconut Fish Curry",
+        "description":"Have you been to Kuala Lumpur with this carefully cooked fish curry. Our chef will take you there, served with steamed vegetables and customers choice of fries, mashed potatoes or steamed rice",
+        "price":38000
+    },
+    {
+        "id":3,
+        "name":"Classic Chicken Korma",
+        "description":"This dish dates and takes you back in time, early 1800s in india, where our chef carefully cooks chicken cubes in a curry sauce served with steamed vegetables and customers choice of fries, mashed potatoes or steamed rice",
+        "price":38000
+    },
+    {
+        "id":4,
+        "name":"Asian Stir Fry",
+        "description":"Have you ever been to Asia? here is your free ticket delivered in original stir fry of steak, chicken or pork served with steamed veggies",
+        "price":42000
+    }
+];
+
+export const BeefSteak = [
+    {
+        "id":1,
+        "name":"Regular Beef Steak",
+        "description":"A tender and juicy beef fillet steak marinated with a blend of selected herbs and spices to meet the customer's choice; served with salads and gravy",
+        "price":38000
+    },
+    {
+        "id":2,
+        "name":"Mushroom Steak",
+        "description":"The higher you go the better it becomes, juicy beef fillet steak served with house salads and mushroom sauce",
+        "price":40000
+    },
+    {
+        "id":3,
+        "name":"Pepper Steak",
+        "description":"Ok it doesn't get any spicer than this juicy beef fillet steak served with house salads and pepper sauce",
+        "price":40000
+    }
+];
+
+export const Pork = [
+    {
+        "id":1,
+        "name":"Tenda Barbeque Spare Ribs",
+        "description":"Yes you had me right, pork ribs, help yourslef with our spare ribs slathered in our signature homemade BBQ sauce served with house salds",
+        "price":42000
+    },
+    {
+        "id":2,
+        "name":"Shanghai Style Pork Chops",
+        "description":"This is straight from china and juicy span-seared pork chop, cooked to perfection with asian herbs and spices served with steamed veggies and gravy",
+        "price":42000
+    }
+];
+
+export const Chicken = [
+    {
+        "id":1,
+        "name":"Tenda Rotisserie Chicken",
+        "description":"This roasted chicken seasoned with delicious herbs and spices, will always make life easy for you served with house salads and gravy",
+        "price":38000
+    },
+    {
+        "id":2,
+        "name":"Regular Chicken Breast",
+        "description":"Taste our mouthwatering three piece chicken breast served with house gravy and side salad",
+        "price":38000
+    },
+    {
+        "id":3,
+        "name":"Mushroom Chicken Breast",
+        "description":"From mouthwatering to mash the room the classic three piece chicken breast served with mushroom sauce",
+        "price":40000
+    },
+    {
+        "id":4,
+        "name":"Pepper Chicken Breast",
+        "description":"Ok let's spice-up things here with our three piece juicy tender chicken breast grilled to perfection served with house side salads and pepper sauce",
+        "price":40000
+    },
+    {
+        "id":5,
+        "name":"Chicken Nuggets",
+        "description":"Just try to hold still while enjoying on these tasty golden breaded chicken nuggets served with salad and tartar sauce",
+        "price":34000
+    },
+    {
+        "id":6,
+        "name":"Tenda BBQ Chicken Wings",
+        "description":"Just uncomplicated yummies - its grilled mouthwatering wings tossed in homemade BBQ sauce served with steamed vegetable, fries and dressing",
+        "price":38000
+    },
+    {
+        "id":7,
+        "name":"Maryland Chicken",
+        "description":"Yummies brought to the table, this breaded baby will make you go Oohh, served salads, fries and dressing",
+        "price":38000
+    }
+];
+
+export const Pizza = [
+    {
+        "id":1,
+        "name":"Classic Chicken Pizza",
+        "description":"Our famous combination of chicken, green pepper, onions, tomatoes on a base of tomato sauce topped with black olives and oregano",
+        "price":26000,
+        "price2":36000
+    },
+    {
+        "id":2,
+        "name":"Classic Bologna Beef Pizza",
+        "description":"For the beef lover, this combination of minced beef, green pepper with black olives and oregano",
+        "price":26000,
+        "price2":36000
+    },
+    {
+        "id":3,
+        "name":"Margarita Basilica Pizza",
+        "description":"Go back to where it all began with classic of tomato base basil leaves and mozzarella cheese and oregano",
+        "price":26000,
+        "price2":36000
+    },
+    {
+        "id":4,
+        "name":"Barbeque Chicken Pizza",
+        "description":"This combination of chicken, green bell pepper, onions, tomatoes and mushrooms, oregano on a base of BBQ sauce and mozzarella cheese",
+        "price":27000,
+        "price2":37000
+    },
+    {
+        "id":5,
+        "name":"Barbeque Beef Pizza",
+        "description":"This combination of beef, green bell pepper, onions, tomatoes and mushrooms, oregano on a base of BBQ sauce and mozzarella cheese",
+        "price":27000,
+        "price2":37000
+    },
+    {
+        "id":6,
+        "name":"Hawaii Honolulu Pizza",
+        "description":"From Honolulu comes your choice of beef, ham or chicken with a combination of pineapple cubes, greenpepper, oregano and mozzarella on a base of tomato sauce",
+        "price":27000,
+        "price2":37000
+    },
+    {
+        "id":7,
+        "name":"Cafe Tenda Special Vegetable Pizza",
+        "description":"For the vegans in the house, this is your kind of thing, a combination of mushrooms, sweetcorn, onions, greenpepper, tomatoes, oregano and mozzarella cheese on a base of tomato sauce topped with green olives",
+        "price":27000,
+        "price2":37000
+    },
+    {
+        "id":8,
+        "name":"Cafe Tenda Special Mixed Pizza",
+        "description":"We always save the best for the last, this combination of chicken, beef, bacon, sausage, onions, green pepper, tomatoes, oregano and mozzarella cheese on a tomato base topped with green olives",
+        "price":30000,
+        "price2":40000
+    }
+];
+
+export const IceCreams = [
+    {
+        "id":1,
+        "name":"Single Scoop",
+        "price":6000
+    },
+    {
+        "id":2,
+        "name":"Double Scoop",
+        "price":9000
+    },
+    {
+        "id":3,
+        "name":"Hot Fudge Sundane",
+        "price":16000
+    },
+    {
+        "id":4,
+        "name":"Banana Split Sundane",
+        "price":66000
+    }
+];
+
+export const KidsMenu = [
+    {
+        "id":1,
+        "name":"Sausage & Chips",
+        "price":18000
+    },
+    {
+        "id":2,
+        "name":"Kiddy Fish Fingers",
+        "price":20000
+    },
+    {
+        "id":3,
+        "name":"Kiddy Chicken Nuggets",
+        "price":20000
+    },
+    {
+        "id":4,
+        "name":"Grilled Cheese Sandiwch",
+        "price":20000
+    },
+    {
+        "id":5,
+        "name":"Kiddy Beef/Chicken Burger",
+        "price":20000
+    }
 ];
